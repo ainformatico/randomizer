acfc18de412b09570dafcdc814b4acb5c8a59cb7
Alejandro El Informatico <aeinformatico@gmail.com>
2014-09-29 23:17:54 +0200

    CHANGE improve code and tests for CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,42 @@
 class CommentsController < ApplicationController
 
   def new
+    render status: :not_found unless params[:tipp_id]
+
     @tipp = Tipp.find_by(name: params[:tipp_id])
     @comment = Comment.new
   end
 
   def create
+    return render status: :not_found unless comment_params[:tipp_id]
 
     @comment = Comment.new(comment_params)
     @tipp = Tipp.find(@comment.tipp_id)
     user = User.find(@comment.user_id)
-
     tipp_owner = User.find(@tipp.user_id)
 
     if @comment.save
-      @tipp.update_points("commented", user.role)
-      user.update_status("commented")
-      tipp_owner.update_status("vouched_for")
-
-      redirect_to new_tipp_path, :flash => { :notice => "Thanks for your Comment!"}
+      update_after_save(@tipp, user, tipp_owner)
+      redirect_to new_tipp_path, flash: { notice: 'Thanks for your Comment!'}
     else
-      flash.now[:error] = "Please make sure that your comment is in between 4 and 140 characters"
-      render  new_comment_path, :tipp => @tipp.name 
+      error_message_for_length
+      render  new_comment_path, tipp: @tipp.name
     end
   end
 
   private
 
+  def error_message_for_length
+    flash.now[:error] = 'Please make sure that your comment is in between 4 and 140 characters'
+  end
+
+  def update_after_save(tipp, user, owner)
+    tipp.update_points('commented', user.role)
+    user.update_status('commented')
+    owner.update_status('vouched_for')
+  end
+
   def comment_params
     params.require(:comment).permit(:content, :user_id, :tipp_id)
   end
 end
-
-
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,12 +3,8 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
 
   describe 'If User is logged out...' do
-
-    it 'is directed to Signup' do
+    it 'is redirected to signup when accessing the #new form' do
       get :new
-      expect(response).to redirect_to(new_user_session_path)
-
-      get :create
       expect(response).to redirect_to(new_user_session_path)
     end
   end
@@ -16,40 +12,70 @@ RSpec.describe CommentsController, type: :controller do
   describe 'If User is logged in..' do
 
     before(:each) do
-      user = FactoryGirl.create(:user)
-      sign_in user
+      sign_in FactoryGirl.create(:user)
     end
 
-    it 'is shown the comment form' do
-      get :new
-      expect(response).to render_template('new')
+    describe '#new' do
+      it 'renders not_found if not tip provided' do
+        get :new
+        expect(response).to be_not_found
+      end
+
+      it 'shows page if tipp provided' do
+        tip = FactoryGirl.create(:tipp)
+        get :new, tipp_id: tip.id
+        expect(assigns(:comment)).to be_an(Comment)
+        expect(response).to render_template('new')
+        expect(response).to be_success
+      end
     end
   end
 
   describe 'when creating a correct Comment...' do
-    before(:each) do
-      owner = FactoryGirl.create(:user)
-      @tipp = FactoryGirl.create(:tipp, user_id: owner.id)
-      @user = FactoryGirl.create(:user, karma: 10)
-      sign_in @user
+    describe '#create' do
+      it 'it can create a correct comment' do
+        create_post_request
+        expect(response).to redirect_to(new_tipp_path)
+      end
 
-      post :create, comment: {content: 'I love their Tacos!', user_id: @user.id, tipp_id: @tipp.id}
-    end
+      it 'Notices the user of the success' do
+        create_post_request
+        expect(flash[:notice]).to be_truthy
+      end
 
-    it 'it can create a correct comment' do
-      expect(response).to redirect_to(new_tipp_path)
-    end
+      it 'relates the comment to a Tipp' do
+        create_post_request
+        expect(Comment.last.tipp_id).to eq(@tipp.id)
+      end
 
-    it 'Notices the user of the success' do
-      expect(flash[:notice]).to be_truthy
-    end
+      it 'relates the comment to a User' do
+        create_post_request
+        expect(Comment.last.user_id).to eq(@user.id)
+      end
 
-    it 'relates the comment to a Tipp' do
-      expect(Comment.last.tipp_id).to eq(@tipp.id)
-    end
+      it 'should render not_found if invalid tip' do
+        create_post_request(tipp_id: nil)
+        expect(response).to be_not_found
+      end
 
-    it 'relates the comment to a User' do
-      expect(Comment.last.user_id).to eq(@user.id)
+      private
+
+      def create_post_request(opts = {})
+        owner = FactoryGirl.create(:user)
+        @tipp = FactoryGirl.create(:tipp, user: owner)
+        @user = FactoryGirl.create(:user, karma: 10)
+        sign_in @user
+
+        options = {
+          content: 'I love their Tacos!',
+          user_id: @user.id,
+          tipp_id: @tipp.id
+        }.merge!(opts)
+
+        comment_attrs = FactoryGirl.attributes_for(:comment, options)
+
+        post :create, comment: comment_attrs
+      end
     end
   end
 end
